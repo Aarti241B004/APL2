6c790d5d4e6825feb3a3ae691888fb86c6d68b7f
Aarti <241B004@juetguna.in>
2026-08-16 12:11:41 +0530

    OOPS concept in java

---
+++ lab exercises/LAB2/constructor/Child.java
@@ -0,0 +1,9 @@
+class Child extends Mother {
+    int age;
+    Child(String name, int age) {
+        super(name);
+        this.age = age;
+        System.out.println("Child constructor called");
+        System.out.println("Child age: " + age);
+    }
+}
---
+++ lab exercises/LAB2/function overloading/Mother.java
@@ -0,0 +1,8 @@
+class Mother {
+    void show() {
+        System.out.println("Mother show");
+    }
+    void show(String name) {
+        System.out.println("Mother show(String): " + name);
+    }
+}
